1672991eff4e22f4e8992088b18880881f00e1aa
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-22 15:01:33 +0100

    logic js

---
+++ script/script.js
@@ -0,0 +1,11 @@
+// seleziono gli elementi di output in pagina
+
+
+//creo costante in cui salvare il risultato del ciclo
+
+//apro ciclo for per generare mail causali
+
+//faccio partire la richiesta Ajax all'api per ricevere indietro la mail generata
+
+
+//inserisco in pagina il risultato 
