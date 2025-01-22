a2d8b9564f47ede5681b6977a8899e5da7bf208d
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-22 15:39:09 +0100

    add axios and innerhtl

--- script/script.js
+++ script/script.js
@@ -5,11 +5,22 @@ const list = document.querySelector('.list-group');
 for (let i = 0; i < 10; i++){
 
     //faccio partire la richiesta Ajax all'api per ricevere indietro la mail generata
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response => {
+        // console.log(response);
+        const email = response.data.response;
+        console.log(email);
+        
+        //inserisco in pagina il risultato 
+        list.innerHTML += `<li>${email}</li>`;
+        
+        
+    })  //altrimenti genera un errore
+        .catch(error => {
+        
 
+    });
     
-    //inserisco in pagina il risultato 
-
-
 
 };
 
