d424739bdc29756a79e1cb353ea8b4e3af0ad007
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-22 15:29:11 +0100

    circle loop

--- script/script.js
+++ script/script.js
@@ -1,11 +1,18 @@
 // seleziono gli elementi di output in pagina
-
-
-//creo costante in cui salvare il risultato del ciclo
+const list = document.querySelector('.list-group');
 
 //apro ciclo for per generare mail causali
+for (let i = 0; i < 10; i++){
 
-//faccio partire la richiesta Ajax all'api per ricevere indietro la mail generata
+    //faccio partire la richiesta Ajax all'api per ricevere indietro la mail generata
+
+    
+    //inserisco in pagina il risultato 
 
 
-//inserisco in pagina il risultato 
+
+};
+
+
+
+
